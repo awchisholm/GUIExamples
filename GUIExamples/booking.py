82a694b6a03ed415fa5b5fcd3f3e181ca3026ea4
Andrew Chisholm <awchisholm@gmail.com>
2021-06-30 22:29:48 +0100

    add password

--- GUIExamples/booking.py
+++ GUIExamples/booking.py
@@ -2,24 +2,49 @@ from guizero import App, Text, TextBox, PushButton, info, Window
 import sql_handling 
 
 db = "booking.db"
-conn = sql_handling.create_connection(db)
-rows = sql_handling.query_connection(conn, "select * from users")
-found = str(len(rows))
+loggedin = False
+user = ""
+
+def show_login_status():
+    if loggedin == True:
+        loginstatus.bg = 'green'
+        loginstatus.value = user
+    else:
+        loginstatus.bg = 'red'
+        loginstatus.value = "select login"
 
 def check_password():
+    global loggedin
+    global user
     conn = sql_handling.create_connection(db)
     query = "select password from users where username = '{0}' and password = '{1}'".format(username.value, password.value)
     rows = sql_handling.query_connection(conn, query)
-    logged_in = False
+    loggedin = False
+    user = ''
     if len(rows) == 1:
-       info("the", "same")
-       logged_in = True
-    return logged_in
+       #info("the", "same")
+       loggedin = True
+       user = username.value
+    
+    show_login_status()
+    return
 
-app = App(title=found)
-text = Text(app, text="Enter password")
+def logout():
+    global loggedin
+    global user
+    loggedin = False
+    username.value = ''
+    password.value = ''
+    user = ''
+    show_login_status()
+
+app = App(title="Booking")
+loginstatus = Text(app)
+show_login_status()
+
 username = TextBox(app)
 password = TextBox(app, hide_text=True)
-button6 = PushButton(app, command=check_password)
+login_button = PushButton(app, command=check_password, text = 'Login')
+logout_button = PushButton(app, command = logout, text = 'Logout')
 
 app.display()
