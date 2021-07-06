b36dc95db7289b6e7606d03ebbd6ce6b8bc4164d
Andrew Chisholm <awchisholm@gmail.com>
2021-07-06 12:36:41 +0100

    working

--- GUIExamples/adduser_example.py
+++ GUIExamples/adduser_example.py
@@ -6,80 +6,60 @@ import create_booking_db
 db = 'adduser.db'
 sql = 'create_adduser.sql'
 
-loggedin = False
-username_global = ""
-
-def show_login_status():
-    global username_global
-    if loggedin == True:
-        loginstatus.bg = 'green'
-        loginstatus.value = username_global
-    else:
-        loginstatus.bg = 'red'
-        loginstatus.value = "select login"
-
 def login():
-    global loggedin
-    global username_global
-    print('login')
+    print('*** login')
     conn = sql_handling.create_connection(db)
     #m = hashlib.sha256()
     #m.update(password.value.encode())
     #hashed_password = m.hexdigest()
     query = "select password from users where username = '{0}' and password = '{1}'".format(username_main.value, password_main.value)
-    print(query)
     rows = sql_handling.query_connection(conn, query)
     conn.close()
-    loggedin = False
-    username_global = ''
     if len(rows) == 1:
         # We have logged in 
         print('logged in')
-        loggedin = True
-        username_global = username_main.value
         addentry_window.show(wait=True)
-
+    else:
+        password_main.value = ''
+        username_main.value = ''
+    oldentries.value = getoldentries()
     return
 
 def logout():
-    print('logout')
-
-def adduser():
-    print('adduser')
-    newuserwindow.show(wait=True)
-    #newuserwindow.visible=True
-    #app.disable = True
+    print('*** logout')
+    username_newuser.value = ''
+    password_newuser.value = ''
 
 def add():
-    print('add')
+    print('*** add')
+    newuserwindow.show(wait=True)
+
+def adduser():
+    print('*** adduser')
     conn = sql_handling.create_connection(db)
     sql_statement = "insert into users (username, password) values ('{0}', '{1}')".format(username_newuser.value, password_newuser.value)
-    print(sql_statement)
     rows = sql_handling.execute_sql(conn, sql_statement)
-    print(rows)
     newuserwindow.hide()
 
-
 def cancel():
-    print('cancel')
+    print('*** cancel')
     newuserwindow.hide()
 
 def getoldentries():
+    print('*** getoldentries')
     conn = sql_handling.create_connection(db)
-    query = "select entry from entries"
+    query = "select entry from entries where username = '{0}'".format(username_main.value)
     rows = sql_handling.query_connection(conn, query)
-    print(rows)
     result = ''.join([str(item[0])+'\n' for item in rows])  # convert list to string with a newline between each
-    print(result)
     return result
 
 def addentry():
-    print("addentry")
+    print("*** addentry")
+    oldentries.value = getoldentries()
+    addentry_window.show(wait=True)
     conn = sql_handling.create_connection(db)
-    sql_statement = "insert into entries (username, entry) values ('{0}', '{1}')".format(username_newuser.value, newentry_window.value)
-    print(sql_statement)
+    sql_statement = "insert into entries (username, entry) values ('{0}', '{1}')".format(username_main.value, newentry_window.value)
     rows = sql_handling.execute_sql(conn, sql_statement)
-    print(rows)
     oldentries.value = getoldentries()
 
 create_booking_db.delete_db(db)
@@ -92,14 +72,14 @@ username_main = TextBox(app, grid=[1,0])
 passprompt_main = Text(app, text='password', grid=[0,1])
 password_main = TextBox(app, hide_text=True, grid=[1,1])
 loginbutton_main = PushButton(app, command = login, text = 'Login', grid=[2,0])
-logoutbutton_main = PushButton(app, command = adduser, text = 'Add user', grid=[3,0])
+logoutbutton_main = PushButton(app, command = add, text = 'Add user', grid=[3,0])
 
 newuserwindow = Window(app, title='New user', visible=False, layout='grid')
 userprompt_newuser = Text(newuserwindow, text='username', grid=[0,0])
-username_newuser = TextBox(newuserwindow, grid=[1,0])
+username_newuser = TextBox(newuserwindow, grid=[1,0], )
 passprompt_newuser = Text(newuserwindow, text='password', grid=[0,1])
 password_newuser = TextBox(newuserwindow, hide_text=True, grid=[1,1])
-loginbutton_newuser = PushButton(newuserwindow, command = add, text = 'Add', grid=[2,0])
+loginbutton_newuser = PushButton(newuserwindow, command = adduser, text = 'Add', grid=[2,0])
 logoutbutton_newuser = PushButton(newuserwindow, command = cancel, text = 'Cancel', grid=[3,0])
 
 addentry_window = Window(app, title='Add an entry', visible = False, layout='grid')
