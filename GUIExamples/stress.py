8a7327aab3603f168a0843859ffffad82952f29c
Andrew Chisholm <awchisholm@gmail.com>
2021-12-29 14:20:38 +0000

    save

--- GUIExamples/stress.py
+++ GUIExamples/stress.py
@@ -1,8 +1,19 @@
 import sql_handling 
+import time
 
 db = "booking_database.db"
 
+insertFlag = True
+
 query = "select * from bookings"
-for index in range(1000000):
-    rows = sql_handling.query_connection(db, query)
-    print(rows)
+insert = "insert into bookings (customerid, slotid, number) values (2, 2, 2)"
+
+for index in range(100000):
+    if insertFlag:
+        sql_handling.execute_sql(db, insert)
+        time.sleep(0.01)
+    else:
+       rows = sql_handling.query_connection(db, query)
+
+#availability = sql_handling.execute_sql(db, query)
+# insert into bookings (customerid, slotid, number) values (2, 2, 2)
